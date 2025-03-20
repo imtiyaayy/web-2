481abb57930b8cf79b2b193aaf927e713db31702
imtiyaayy <imtiyazmufidah08@gmail.com>
2025-03-20 11:32:21 +0700

    praktikum05

--- praktikum/startbootstrap-freelancer-gh-pages/partials/sections/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once "portofolio.php";
-include_once "about.php";
-include_once "contact.php";
--- task/partials/sections/index-section.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once "contact-section.php";
-include_once "education-section.php";
-include_once "experience-section.php";
-include_once "project-section.php";
-include_once "resume-section.php";
-include_once "skills-section.php";
